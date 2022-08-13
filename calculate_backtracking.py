b7ed826e3c36922c6faa9c99407813b1c261f4ec
victoriago1 <victoriago@campus.technion.ac.il>
2022-08-13 14:43:45 +0300

    backtracking implementation

---
+++ calculate_backtracking.py
@@ -0,0 +1,59 @@
+from math import ceil, floor
+
+"""
+Calculates the number of strands that hold the homopolymer constraint
+and the GC/AT 5% almost balanced constraint.
+"""
+
+SIGMA = ['a','t','c','g']
+
+
+def calc_longest_homopolymer(str) -> int:
+    if not str:
+        return 0
+    longest = 1
+    prev = None
+    current_len = 0
+    for char in str:
+        if char == prev:
+            current_len += 1
+            if current_len > longest:
+                longest = current_len
+        else:
+            prev = char
+            current_len = 1
+    return longest
+
+def calc_weight(str):
+    return str.count('a') + str.count('t')
+
+def calc_strands(n, m, verbose=False) -> int:
+    w_min = ceil(0.45 * n)
+    w_max = floor(0.55 * n)
+    if w_max < w_min:
+        w_min = w_max = n//2  # by definition of balanced in Knuth
+    return calc_strands_wrapper(n=n, m=m, w_min=w_min, w_max=w_max, str='', verbose=verbose)
+
+def calc_strands_wrapper(n, m, w_min, w_max, str, verbose) -> int:
+    if calc_longest_homopolymer(str) > m:
+        return 0
+
+    if len(str) == n:
+        weight = calc_weight(str)
+        if weight >= w_min and weight <= w_max:
+            if verbose:
+                print(str)
+            return 1
+        return 0
+    
+    counter = 0
+    for char in SIGMA:
+        counter += calc_strands_wrapper(n=n, m=m, w_min=w_min, w_max=w_max, str=str + char, verbose=verbose)
+    return counter
+
+if __name__ == "__main__":
+    # print(calc_strands(n=4, m=3, verbose=True))
+    m = 3
+    for n in range(2, 13):
+        print('Number of strands for n={} and m={}: {}'.format(n, m, calc_strands(n=n, m=m, verbose=False)))
+    
