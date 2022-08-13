518963123a705e355ab3f38badff9c223072aea9
victoriago1 <victoriago@campus.technion.ac.il>
2022-08-13 15:00:57 +0300

    added documentation

--- AlmostBalancedBaselineRedundancy.py
+++ AlmostBalancedBaselineRedundancy.py
@@ -1,16 +1,25 @@
 from math import comb, log2, ceil, floor
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
+
+"""
+Calculates the number of bits for minimum and optimal redundancy, in case of almost balanced code,
+without the constraint of GC/AT content.
+This is done by counting the number of the strands that hold this condition, and calculating the
+number of binary bits that can express the same number of strands, and returning the difference between the two.
+In the created CSV, the columns are the value of n and its redundancy.
+"""
 
 if __name__ == "__main__":
     N = 1000
     start = 2
     arr = np.empty((N,2))
-    for n in range(start, N+start):
+    for n in tqdm(range(start, N+start)):
         arr_n_index = n - start
-        print(n)
         sum = np.sum([comb(n, i) for i in range(ceil(0.45*n), floor(0.55*n) + 1, 1)])
-        log_sum = floor(log2(sum)) if sum>0 else floor(log2(comb(n, n//2)))  # in case of odd n, by definition of exactly balanced (Knuth)
+        # weight is exactly n//2 in case of odd n and small values (and then sum == 0), by definition of exactly balanced (Knuth)
+        log_sum = floor(log2(sum)) if sum>0 else floor(log2(comb(n, n//2))) 
         arr[arr_n_index][0] = n
         arr[arr_n_index][1] = n - log_sum
     
--- calculate_backtracking.py
+++ calculate_backtracking.py
@@ -1,7 +1,7 @@
 from math import ceil, floor
 
 """
-Calculates the number of strands that hold the homopolymer constraint
+Calculates by backtracking the number of strands that hold the homopolymer constraint
 and the GC/AT 5% almost balanced constraint.
 """
 
@@ -9,8 +9,12 @@ SIGMA = ['a','t','c','g']
 
 
 def calc_longest_homopolymer(str) -> int:
+
+    """ Returns the maximum homopolymer length. """
+
     if not str:
         return 0
+    
     longest = 1
     prev = None
     current_len = 0
@@ -24,17 +28,27 @@ def calc_longest_homopolymer(str) -> int:
             current_len = 1
     return longest
 
+
 def calc_weight(str):
+
+    """ Returns the wheight of the strand. """
+
     return str.count('a') + str.count('t')
 
+
 def calc_strands(n, m, verbose=False) -> int:
+
+    """ Returns the number of strands of length n and maximum homopolymer length of m.
+        If verbose==True, all of the strands that hold the conditions will be printed as well. """
+    
     w_min = ceil(0.45 * n)
     w_max = floor(0.55 * n)
     if w_max < w_min:
         w_min = w_max = n//2  # by definition of balanced in Knuth
-    return calc_strands_wrapper(n=n, m=m, w_min=w_min, w_max=w_max, str='', verbose=verbose)
+    return _calc_strands_wrapper(n=n, m=m, w_min=w_min, w_max=w_max, str='', verbose=verbose)
 
-def calc_strands_wrapper(n, m, w_min, w_max, str, verbose) -> int:
+
+def _calc_strands_wrapper(n, m, w_min, w_max, str, verbose) -> int:
     if calc_longest_homopolymer(str) > m:
         return 0
 
@@ -48,11 +62,13 @@ def calc_strands_wrapper(n, m, w_min, w_max, str, verbose) -> int:
     
     counter = 0
     for char in SIGMA:
-        counter += calc_strands_wrapper(n=n, m=m, w_min=w_min, w_max=w_max, str=str + char, verbose=verbose)
+        counter += _calc_strands_wrapper(n=n, m=m, w_min=w_min, w_max=w_max, str=str + char, verbose=verbose)
     return counter
+
 
 if __name__ == "__main__":
     # print(calc_strands(n=4, m=3, verbose=True))
+
     m = 3
     for n in range(2, 13):
         print('Number of strands for n={} and m={}: {}'.format(n, m, calc_strands(n=n, m=m, verbose=False)))
