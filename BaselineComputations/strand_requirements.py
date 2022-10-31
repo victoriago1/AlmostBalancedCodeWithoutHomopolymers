cdf2abc40770ddbe91a4ad6ed96374569d8c8770
gili.d <gili.d@campus.technion.ac.il>
2022-10-31 02:26:00 +0200

    Update strand_requirements for final computations

--- BaselineComputations/strand_requirements.py
+++ BaselineComputations/strand_requirements.py
@@ -1,11 +1,11 @@
 from math import ceil, floor
 
-REDUNDANCY_LIMIT_PERCENTAGE = 0
-REDUNDANCY_LIMIT_LETTERS = 5
-IMBALANCE_PERCENTAGE = 0 # The acceptable imbalance of each strand in percentages
+REDUNDANCY_LIMIT_PERCENTAGE = 8
+REDUNDANCY_LIMIT_LETTERS = 0
+IMBALANCE_PERCENTAGE = 5 # The acceptable imbalance of each strand in percentages
 m = 3 # Longest homopolymer run
-MAX_n_binary = 100000 # Longest binary string length
-MAX_n_quaternary = ceil((MAX_n_binary/2.0) + ((0.01*REDUNDANCY_LIMIT_PERCENTAGE)*MAX_n_binary)) +\
+MAX_n_binary = 1000 # Longest binary string length
+MAX_n_quaternary = ceil((MAX_n_binary/2.0) + ((0.01*REDUNDANCY_LIMIT_PERCENTAGE/2)*MAX_n_binary)) +\
                    REDUNDANCY_LIMIT_LETTERS # Longest strand length
 MAX_w = floor((0.5 + (0.01*IMBALANCE_PERCENTAGE))*MAX_n_quaternary) # Maximum weight of the longest strand
                                                          # (used to restrict unnecessary calculations)
